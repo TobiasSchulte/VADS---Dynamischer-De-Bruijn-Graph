6c553cba135bc6284d4bb4c0450081cc9d1f7f81
Tobias Schulte <toschulte@gmail.com>
2011-07-14 15:46:29 +0200

    libraries

---
+++ src/start.cpp
@@ -0,0 +1,22 @@
+/*
+ * start.cpp
+ *
+ *  Created on: 14.07.2011
+ *      Author: tobias
+ */
+
+#include <iostream>
+
+#include "../include/sha1.h"
+#include "../include/Subjects1-6.h"
+
+using namespace std;
+
+int main(int argc, char* argv){
+
+	//int64 i;
+
+	cout << "bla";
+
+	return 1;
+}
